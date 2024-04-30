30690f864c26cf685d27d2d61fb5d85fdc52ea6a
Chenghan0626 <164443684+Chenghan0626@users.noreply.github.com>
2024-04-30 23:33:40 +0800

    Create CardTypeEvaluator.h

---
+++ CardTypeEvaluator.h
@@ -0,0 +1,26 @@
+#ifndef CARDTYPEEVALUATOR_H
+#define CARDTYPEEVALUATOR_H
+
+struct Card {
+    int value;
+    char suit;
+};
+
+class CardTypeEvaluator {
+public:
+    static void sortHand(Card* hand);
+    static const char* evaluateHand(Card* hand);
+
+private:
+    static bool isRoyalFlush(const Card* hand);
+    static bool isStraightFlush(const Card* hand);
+    static bool isFourOfAKind(const Card* hand);
+    static bool isFullHouse(const Card* hand);
+    static bool isFlush(const Card* hand);
+    static bool isStraight(const Card* hand);
+    static bool isThreeOfAKind(const Card* hand);
+    static bool isTwoPair(const Card* hand);
+    static bool isPair(const Card* hand);
+};
+
+#endif
