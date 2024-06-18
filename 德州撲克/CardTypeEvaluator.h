4da480841e420e4c510d2da2610cad3cde6026d3
Chenghan0626 <164443684+Chenghan0626@users.noreply.github.com>
2024-06-18 10:22:32 +0800

    Update and rename CardTypeEvaluator.h to 德州撲克/CardTypeEvaluator.h

--- 德州撲克/CardTypeEvaluator.h
+++ 德州撲克/CardTypeEvaluator.h
@@ -1,16 +1,14 @@
 #ifndef CARDTYPEEVALUATOR_H
 #define CARDTYPEEVALUATOR_H
-
 struct Card {
     int value;
     char suit;
 };
-
 class CardTypeEvaluator {
 public:
     static void sortHand(Card* hand);
     static const char* evaluateHand(Card* hand);
-
+    static int getHandRank(const char* type);
 private:
     static bool isRoyalFlush(const Card* hand);
     static bool isStraightFlush(const Card* hand);
@@ -22,5 +20,4 @@ private:
     static bool isTwoPair(const Card* hand);
     static bool isPair(const Card* hand);
 };
-
 #endif
